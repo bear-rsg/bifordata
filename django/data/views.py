68c26b82bec9abad87119c3ebf94edf8382de857
Mike Allaway <m.j.allaway@bham.ac.uk>
2021-09-14 21:50:51 +0100

    tidy view

--- django/data/views.py
+++ django/data/views.py
@@ -22,13 +22,10 @@ class DataListView(ListView):
         Also add other objects, e.g. DataLinkCategories, to use in the page
         """
 
-        # Start with all published objects, which may be filtered/ordered below if such data is included in the request
+        # Start with all published objects,
         queryset = self.model.objects.filter(admin_published=True)
 
-        #
         # Search
-        #
-
         search = self.request.GET.get('search', '')
         if search != '':
             queryset = queryset.filter(
@@ -39,28 +36,21 @@ class DataListView(ListView):
                 Q(category__name__contains=search)
             )
 
-        #
         # Filter
-        #
-
-        # Many to One relationship filters
         filter_category = self.request.GET.get('filter_category', '')
         if filter_category != '':
             queryset = queryset.filter(category=filter_category)
 
-        #
+
         # Order
-        #
-
-        # Establish the order direction (asc/desc) and the field to order by, from the request
         order = self.request.GET.get('order_direction', '') + self.request.GET.get('order_by', 'name')
-
         # If starts with a '-' then it means order descending
         if order[0] == '-':
             queryset = queryset.order_by(Lower(order[1:]).desc())
         else:
             queryset = queryset.order_by(Lower(order))
 
+        # Return the searched, filtered, and ordered queryset
         return queryset
 
     def get_context_data(self, **kwargs):
