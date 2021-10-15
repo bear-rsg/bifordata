d5854e60093c3e9f39c0a235cf1e1b4869d9af76
AHR Software <artsresearchsoftware@gmail.com>
2021-10-15 10:18:45 +0100

    moved dataroot into its own file

---
+++ django/core/local_settings_dataroot.example.py
@@ -0,0 +1,10 @@
+"""
+This file is a local settings file that's specifically used
+to store setting(s) about the data directory.
+
+It's kept separate from the general local_settings.py file
+so that it can be more easily managed by Salt.
+"""
+
+# The path of the root data folder
+DATA_ROOT = "/path/to/local/data/folder"
